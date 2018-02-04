b26ac3689d04c1243db51df15f5455739ebdbe68
bassbone <bassbone0o_o0@yahoo.co.jp>
2018-02-04 07:38:45 +0100

    骨組み完成

--- qasite/src/dependencies.php
+++ qasite/src/dependencies.php
@@ -17,3 +17,21 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+// twig
+$container['twig'] = function ($c) {
+    $settings = $c->get('settings')['renderer'];
+    $view = new \Slim\Views\Twig($settings['template_path']);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
+
+// redis
+$container['redis'] = function ($c) {
+    return new Predis\Client('tcp://127.0.0.1:6379');
+};
+
--- qasite/src/routes.php
+++ qasite/src/routes.php
@@ -5,10 +5,43 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $redis = $this->redis;
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    $view_value = [];
+
+    $q_all = $redis->lrange('q', 0, -1);
+    $q_all = array_reverse($q_all);
+    $len_q = count($q_all);
+    
+    foreach ($q_all as $index => $q) {
+        $a = $redis->lrange('a_'.($len_q - $index), 0, -1);
+        $view_value[] = [
+            'q_id' => $len_q - $index,
+            'q' => $q,
+            'a' => $a
+        ];
+    }
+
+    return $this->twig->render($response, 'index.html', ['view_value' => $view_value]);
 });
+
+$app->post('/answer', function (Request $request, Response $response, array $args) {
+    $redis = $this->redis;
+
+    $q_id = $request->getQueryParams()['q'];
+    $answer = $request->getParsedBody()['answer'];
+    $redis->rpush('a_'.$q_id, $answer);
+
+    return $response->withStatus(302)->withHeader('Location', '/');
+});
+
+$app->post('/question', function (Request $request, Response $response, array $args) {
+    $redis = $this->redis;
+
+    $question = $request->getParsedBody()['question'];
+    $redis->rpush('q', $question);
+
+    return $response->withStatus(302)->withHeader('Location', '/');
+});
+
